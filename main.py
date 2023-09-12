5f9acaa54f6794234cc2640cc108e25f8278f844
Kitamo <102401275+Kitamog@users.noreply.github.com>
2023-09-12 17:25:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,96 +38,95 @@ async def send_map_every_30_seconds():
 				print("(ALS)Request succeeded")
 			else:
 				print("(ALS)Request failed with " + str(res_map.status_code))
+
+			# カジュアルのマップローテーションを取得
+			battle_royale_current_map = json_map['battle_royale']['current']['map']
+			battle_royale_current_map_start = json_map['battle_royale']['current']['readableDate_start']
+			battle_royale_current_map_end = json_map['battle_royale']['current']['readableDate_end']
+			battle_royale_next_map = json_map['battle_royale']['next']['map']
+			battle_royale_next_map_start = json_map['battle_royale']['next']['readableDate_start']
+			battle_royale_next_map_end = json_map['battle_royale']['next']['readableDate_end']
+			# ランクのマップローテーションを取得
+			ranked_current_map = json_map['ranked']['current']['map']
+			ranked_current_map_start = json_map['ranked']['current']['readableDate_start']
+			ranked_current_map_end = json_map['ranked']['current']['readableDate_end']
+			ranked_next_map = json_map['ranked']['next']['map']
+			ranked_next_map_start = json_map['ranked']['next']['readableDate_start']
+			ranked_next_map_end = json_map['ranked']['next']['readableDate_end']
+	
+			# カジュアルをJSTに変換
+			battle_royale_current_map_start_jst = datetime.strptime(
+				battle_royale_current_map_start, '%Y-%m-%d %H:%M:%S')
+			battle_royale_current_map_start_jst = battle_royale_current_map_start_jst + timedelta(hours=9)
+			battle_royale_current_map_end_jst = datetime.strptime(
+				battle_royale_current_map_end, '%Y-%m-%d %H:%M:%S')
+			battle_royale_current_map_end_jst = battle_royale_current_map_end_jst + timedelta(hours=9)
+			battle_royale_next_map_start_jst = datetime.strptime(
+				battle_royale_next_map_start, '%Y-%m-%d %H:%M:%S')
+			battle_royale_next_map_start_jst = battle_royale_next_map_start_jst + timedelta(hours=9)
+			battle_royale_next_map_end_jst = datetime.strptime(
+				battle_royale_next_map_end, '%Y-%m-%d %H:%M:%S')
+			battle_royale_next_map_end_jst = battle_royale_next_map_end_jst + timedelta(hours=9)
+	
+			# ランクをJSTに変換
+			ranked_current_map_start_jst = datetime.strptime(
+				ranked_current_map_start, '%Y-%m-%d %H:%M:%S')
+			ranked_current_map_start_jst = ranked_current_map_start_jst + timedelta(hours=9)
+			ranked_current_map_end_jst = datetime.strptime(
+				ranked_current_map_end, '%Y-%m-%d %H:%M:%S')
+			ranked_current_map_end_jst = ranked_current_map_end_jst + timedelta(hours=9)
+			ranked_next_map_start_jst = datetime.strptime(
+				ranked_next_map_start, '%Y-%m-%d %H:%M:%S')
+			ranked_next_map_start_jst = ranked_next_map_start_jst + timedelta(hours=9)
+			ranked_next_map_end_jst = datetime.strptime(
+				ranked_next_map_end, '%Y-%m-%d %H:%M:%S')
+			ranked_next_map_end_jst = ranked_next_map_end_jst + timedelta(hours=9)
+	
+	
+			# メッセージ内容
+			msg_content = (
+			"[カジュアル]\n"
+			+ "現在("
+			+ battle_royale_current_map_start_jst.strftime('%H:%M')
+			+ "～"
+			+ battle_royale_current_map_end_jst.strftime('%H:%M')
+			+ ")⇒"
+			+ map_list[battle_royale_current_map]['name']
+			+ "\n"
+			+ "次("
+			+ battle_royale_next_map_start_jst.strftime('%H:%M')
+			+ "～"
+			+ battle_royale_next_map_end_jst.strftime('%H:%M')
+			+ ")⇒"
+			+ map_list[battle_royale_next_map]['name']
+			+ "\n\n"
+			+ "[ランク] ("
+			+ ranked_next_map_start_jst.strftime('%H:%M')
+			+ "切り替わり)\n"
+			+ "現在⇒"
+			+ map_list[ranked_current_map]['name']
+			+ "\n"
+			+ "次⇒"
+			+ map_list[ranked_next_map]['name']
+			)
+	
+			global msged_content
+	
+			if msged_content == msg_content:
+				print('既にメッセージを送信済みです')
+			else:
+				# メッセージを送信(まだmsg_contentを送っていないとき)
+				for channel in active_channels:
+					target_channel = client.get_channel(channel)
+					if target_channel:
+						await target_channel.send(msg_content)
+					# 送信済メッセージの内容を保存
+					msged_content = msg_content
+					print('メッセージを送信しました')
+			await asyncio.sleep(30)  # 30秒待つ
 		except Exception as e:
 			print(f"エラーが発生しました: {e}")
 			continue  # 最初からやり直す
-
-		# カジュアルのマップローテーションを取得
-		battle_royale_current_map = json_map['battle_royale']['current']['map']
-		battle_royale_current_map_start = json_map['battle_royale']['current']['readableDate_start']
-		battle_royale_current_map_end = json_map['battle_royale']['current']['readableDate_end']
-		battle_royale_next_map = json_map['battle_royale']['next']['map']
-		battle_royale_next_map_start = json_map['battle_royale']['next']['readableDate_start']
-		battle_royale_next_map_end = json_map['battle_royale']['next']['readableDate_end']
-		# ランクのマップローテーションを取得
-		ranked_current_map = json_map['ranked']['current']['map']
-		ranked_current_map_start = json_map['ranked']['current']['readableDate_start']
-		ranked_current_map_end = json_map['ranked']['current']['readableDate_end']
-		ranked_next_map = json_map['ranked']['next']['map']
-		ranked_next_map_start = json_map['ranked']['next']['readableDate_start']
-		ranked_next_map_end = json_map['ranked']['next']['readableDate_end']
-
-		# カジュアルをJSTに変換
-		battle_royale_current_map_start_jst = datetime.strptime(
-			battle_royale_current_map_start, '%Y-%m-%d %H:%M:%S')
-		battle_royale_current_map_start_jst = battle_royale_current_map_start_jst + timedelta(hours=9)
-		battle_royale_current_map_end_jst = datetime.strptime(
-			battle_royale_current_map_end, '%Y-%m-%d %H:%M:%S')
-		battle_royale_current_map_end_jst = battle_royale_current_map_end_jst + timedelta(hours=9)
-		battle_royale_next_map_start_jst = datetime.strptime(
-			battle_royale_next_map_start, '%Y-%m-%d %H:%M:%S')
-		battle_royale_next_map_start_jst = battle_royale_next_map_start_jst + timedelta(hours=9)
-		battle_royale_next_map_end_jst = datetime.strptime(
-			battle_royale_next_map_end, '%Y-%m-%d %H:%M:%S')
-		battle_royale_next_map_end_jst = battle_royale_next_map_end_jst + timedelta(hours=9)
-
-		# ランクをJSTに変換
-		ranked_current_map_start_jst = datetime.strptime(
-			ranked_current_map_start, '%Y-%m-%d %H:%M:%S')
-		ranked_current_map_start_jst = ranked_current_map_start_jst + timedelta(hours=9)
-		ranked_current_map_end_jst = datetime.strptime(
-			ranked_current_map_end, '%Y-%m-%d %H:%M:%S')
-		ranked_current_map_end_jst = ranked_current_map_end_jst + timedelta(hours=9)
-		ranked_next_map_start_jst = datetime.strptime(
-			ranked_next_map_start, '%Y-%m-%d %H:%M:%S')
-		ranked_next_map_start_jst = ranked_next_map_start_jst + timedelta(hours=9)
-		ranked_next_map_end_jst = datetime.strptime(
-			ranked_next_map_end, '%Y-%m-%d %H:%M:%S')
-		ranked_next_map_end_jst = ranked_next_map_end_jst + timedelta(hours=9)
-
-
-		# メッセージ内容
-		msg_content = (
-		"[カジュアル]\n"
-		+ "現在("
-		+ battle_royale_current_map_start_jst.strftime('%H:%M')
-		+ "～"
-		+ battle_royale_current_map_end_jst.strftime('%H:%M')
-		+ ")⇒"
-		+ map_list[battle_royale_current_map]['name']
-		+ "\n"
-		+ "次("
-		+ battle_royale_next_map_start_jst.strftime('%H:%M')
-		+ "～"
-		+ battle_royale_next_map_end_jst.strftime('%H:%M')
-		+ ")⇒"
-		+ map_list[battle_royale_next_map]['name']
-		+ "\n\n"
-		+ "[ランク] ("
-		+ ranked_next_map_start_jst.strftime('%H:%M')
-		+ "切り替わり)\n"
-		+ "現在⇒"
-		+ map_list[ranked_current_map]['name']
-		+ "\n"
-		+ "次⇒"
-		+ map_list[ranked_next_map]['name']
-		)
-
-		global msged_content
-
-		if msged_content == msg_content:
-			print('既にメッセージを送信済みです')
-		else:
-			# メッセージを送信(まだmsg_contentを送っていないとき)
-			for channel in active_channels:
-				target_channel = client.get_channel(channel)
-				if target_channel:
-					await target_channel.send(msg_content)
-				# 送信済メッセージの内容を保存
-				msged_content = msg_content
-				print('メッセージを送信しました')
-		await asyncio.sleep(30)  # 30秒待つ
-
 @client.event
 async def on_ready():
     print("起動完了")
